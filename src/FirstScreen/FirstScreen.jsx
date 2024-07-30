6ea0b83593c6f4385eb61245c52a7cb38d44beb4
Vladislav Shamray <127450674+PanShamray@users.noreply.github.com>
2024-07-30 19:09:02 +0300

    design part

--- src/FirstScreen/FirstScreen.jsx
+++ src/FirstScreen/FirstScreen.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import './style.scss';
 import SecondScreen from '../SecondScreen/SecondScreen';
+
+import './style.scss';
+import me from '../images/me.jpg'
 
 function FirstScreen () {
     return (
-      <>
-            <h3>hi there 1</h3>
-            <SecondScreen/>
-      </>
+      <div className="main_wrapper">
+        <img src={me} alt=""></img>
+        <p>Am I love you?</p>
+
+        <div className='buttons_wrapper'>
+            <button type='submit'>YES</button>
+            <button type='buttron'>no{"(((((("}</button>
+        </div>
+            
+      </div>
     );
 };
 

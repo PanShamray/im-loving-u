f2e2befae3ddefa2f240fe22251eba36cdae7495
Vladislav Shamray <127450674+PanShamray@users.noreply.github.com>
2024-07-30 18:09:13 +0300

    created a codding macket

---
+++ src/FirstScreen/FirstScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+import SecondScreen from '../SecondScreen/SecondScreen';
+
+function FirstScreen () {
+    return (
+      <>
+            <h3>hi there 1</h3>
+            <SecondScreen/>
+      </>
+    );
+};
+
+export default FirstScreen;
---
+++ src/SecondScreen/SecondScreen.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+function SecondScreen() {
+  return <h6>hi there 2</h6>;
+}
+
+export default SecondScreen;
